d0383d4b9e1a41a077a622c7ffc34bbd12d686c2
Isaac Weintraub <iweintraub3@gatech.edu>
2018-03-01 12:23:37 -0500

    M6 completed on webapp, layout isn't the best

--- webapp/model/ShelterModel.js
+++ webapp/model/ShelterModel.js
@@ -6,10 +6,20 @@ module.exports.shelters = [];
 module.exports.readShelters = function(onCompleted) {
   var rl = readline.createInterface({
     input: fs.createReadStream(__dirname + '/HomelessShelterDatabase.csv'),
-      crlfDelay: Infinity
+    crlfDelay: Infinity,
   });
   rl.on('line', (line) => {
-    module.exports.shelters.push(line.toString().match(/("(.+?)")|([^,]+)/g));
+    var match = line.toString().match(/("(.+?)")|([^,]+)/g);
+    var contains = false;
+    for (var shelter of module.exports.shelters) {
+      if (match[0] == shelter[0]) {
+        contains = true;
+        break;
+      }
+    }
+    if (!contains) {
+      module.exports.shelters.push(match);
+    }
   });
   rl.on('close', (close) => {
     onCompleted();
